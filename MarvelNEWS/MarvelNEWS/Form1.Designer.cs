10e12517b60a3cfa28bd490666d012d8129e8f36
Cristian <cristhian.toro@upb.edu.co>
2018-01-27 16:52:17 -0500

    botones
    iplementación de botones

--- MarvelNEWS/MarvelNEWS/Form1.Designer.cs
+++ MarvelNEWS/MarvelNEWS/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Small", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(54, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Black Panther";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Sitka Small", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(54, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 80);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Avengers: Infinity War";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Sitka Small", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(54, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(209, 80);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ant-Man and The Wasp";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(322, 363);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MARVEL NEWS";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
