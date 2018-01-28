20d6941e82934fbe3cea21acdf51b392e790cee3
Cristian <cristhian.toro@upb.edu.co>
2018-01-28 12:33:38 -0500

    form AntWasp

--- MarvelNEWS/MarvelNEWS/Form1.cs
+++ MarvelNEWS/MarvelNEWS/Form1.cs
@@ -36,5 +36,12 @@ namespace MarvelNEWS
             inf.StartPosition = FormStartPosition.CenterScreen;
             inf.Show();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AntWasp aw = new AntWasp();
+            aw.StartPosition = FormStartPosition.CenterScreen;
+            aw.Show();
+        }
     }
 }
