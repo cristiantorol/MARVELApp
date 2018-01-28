9d8fdc155ab3c9c97c37bd83636e7a68ee4701db
Cristian <cristhian.toro@upb.edu.co>
2018-01-28 13:23:45 -0500

    reparto aw

--- MarvelNEWS/MarvelNEWS/AntWasp.Designer.cs
+++ MarvelNEWS/MarvelNEWS/AntWasp.Designer.cs
@@ -32,6 +32,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,11 +67,34 @@
             this.label2.TabIndex = 3;
             this.label2.Text = resources.GetString("label2.Text");
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Reparto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(256, 52);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Paul Rudd, Evangeline Lilly, Michael Peña\r\nBobby Cannavale, T.I. Harris, David Da" +
+    "stmalchian\r\nJudy Greer, Michael Douglas, Michelle Pfeiffer\r\nHannah John-Kamen, R" +
+    "andall Park, Walton Goggins";
+            // 
             // AntWasp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(519, 471);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -86,5 +111,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
