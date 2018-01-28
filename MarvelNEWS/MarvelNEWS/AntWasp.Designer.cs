63db074a3d14e7063cbaea83231839e7958bc770
Cristian <cristhian.toro@upb.edu.co>
2018-01-28 13:31:49 -0500

    form aw finalizado

--- MarvelNEWS/MarvelNEWS/AntWasp.Designer.cs
+++ MarvelNEWS/MarvelNEWS/AntWasp.Designer.cs
@@ -35,6 +35,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -100,11 +101,22 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Tráiler aún no disponible";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Volver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // AntWasp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(519, 471);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -127,5 +139,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- MarvelNEWS/MarvelNEWS/AntWasp.cs
+++ MarvelNEWS/MarvelNEWS/AntWasp.cs
@@ -16,5 +16,10 @@ namespace MarvelNEWS
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
